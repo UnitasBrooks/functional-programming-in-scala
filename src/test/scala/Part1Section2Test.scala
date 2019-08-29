5785673e9c04b101f54e05f1d1edb9598c0914d4
Joseph Yuhas <yuhasj@amazon.com>
2019-08-29 14:00:06 -0700

    finished unit tests for exercises

--- src/test/scala/Part1Section2Test.scala
+++ src/test/scala/Part1Section2Test.scala
@@ -5,9 +5,12 @@ import org.scalatest.FunSpec
 
 class Part1Section2Test extends FunSpec {
   trait Fixture  {
-    val divideThree: Double => Double = (i: Double) => i / 3
-    val timesTen: Double => Double = (n: Double) => n * 10
+    val divideThree: Int => Int = (i: Int) => i / 3
+    val timesTen: Int => Int = (n: Int) => n * 10
+    val intGreaterThan: (Int, Int) => Boolean = (i: Int, j: Int) => { i >= j}
     val concatInts: (Int, Int) => String = (x: Int, y: Int) => (x.toString + y.toString)
+    val sortedArray: Array[Int] = Array(1, 2, 3, 4, 5, 6)
+    val array: Array[Int] = Array(1, 5, 3, 4, 4, 0)
   }
 
   describe("compose") {
@@ -31,12 +34,44 @@ class Part1Section2Test extends FunSpec {
 
   describe("uncurry") {
     new Fixture {
-      it("it should take two single parameter functions and return a two parameter function") {
+      it("should take two single parameter functions and return a two parameter function") {
         val uncurried = uncurry(curry(concatInts))
         assert(uncurried(1,2) == "12")
       }
     }
   }
+
+  describe("isSorted") {
+    new Fixture {
+      it("should find that the array is sorted when we provide a presorted array of integers") {
+        assert(isSorted(sortedArray, intGreaterThan))
+      }
+
+      it("should find that the arrays is not sorted when provide a unsorted array") {
+        assert(!isSorted(array, intGreaterThan))
+      }
+    }
+  }
+
+  describe("formatResult") {
+    new Fixture {
+      it("should create a string based on the result of a (Int, Int) function") {
+        assert(formatResult("times ten", 3, timesTen) == "The times ten of 3 is 30")
+      }
+    }
+  }
+
+  describe("fib") {
+    it("should return 1 when 1 is supplied") {
+      assert(fib(1) == 1)
+    }
+
+    it("should return 55 when 10 is supplied") {
+      assert(fib(10) == 55)
+    }
+
+  }
+
 
 
 
