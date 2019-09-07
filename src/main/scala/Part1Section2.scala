0bf92e2eecd453955b424d8f24a02b8355549913
UnitasBrooks <joe.yuhas@gmail.com>
2019-09-07 15:47:33 -0700

    stash changes

--- src/main/scala/Part1Section2.scala
+++ src/main/scala/Part1Section2.scala
@@ -16,7 +16,7 @@ object Part1Section2 {
     println(isSorted(Array(1, 2, 2, 3, 5, 5, 6), (a: Int, b: Int) => a >= b))
     println(curryExample(number, (i: Int, s: String) => { s.length.toFloat / i.toFloat })("Hello world!"))
 
-    val concatIntsFunction = (a: Int) => { (b: Int) => { b.toString + a.toString} }
+    val concatIntsFunction = (a: Int) => { b: Int => { b.toString + a.toString} }
 
     val concatFiveCurried = concatIntsFunction(5)
     println(concatFiveCurried(number)) // number + "5"
@@ -36,9 +36,9 @@ object Part1Section2 {
     println(addFive(number)) // number + 5
   }
 
-  def compose[A,B,C](f: B => C, g: A => B): A => C = { (a: A) => f(g(a)) }
+  def compose[A,B,C](f: B => C, g: A => B): A => C = { a: A => f(g(a)) }
 
-  def curry[A,B,C](f: (A, B) => C): (A) => (B => C) = {
+  def curry[A,B,C](f: (A, B) => C): A => (B => C) = {
     (a: A) =>  (b: B) => f(a, b)
   }
 
