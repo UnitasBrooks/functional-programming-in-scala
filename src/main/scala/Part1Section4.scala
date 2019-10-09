6f405f89b5dd9fd2230aaa7378b16d2393e2c589
UnitasBrooks <joe.yuhas@gmail.com>
2019-10-09 16:44:07 -0700

    finished Option, started Either.

--- src/main/scala/Part1Section4.scala
+++ src/main/scala/Part1Section4.scala
@@ -1,15 +1,20 @@
+
+
 object Part1Section4 extends App {
   // Exercise 4.1
-
   sealed trait Option[+A] {
     def map[B](f: A => B): Option[B] = this match {
       case None => None
       case Some(a) => Some(f(a))
     }
 
-    def flatMap[B](f: A => Option[B]): Option[B] = this match {
+    def flatMapMatch[B](f: A => Option[B]): Option[B] = this match {
       case None => None
       case Some(a) => f(a)
+    }
+
+    def flatMap[B](f: A => Option[B]): Option[B] = {
+      this map f getOrElse None
     }
 
     def getOrElse[B >: A](default: => B): B = this match {
@@ -17,19 +22,26 @@ object Part1Section4 extends App {
       case Some(x) => x
     }
 
-    def orElse[B >: A](ob: => Option[B]): Option[B] = {
+    def orElseIf[B >: A](ob: => Option[B]): Option[B] = {
       if(this == None) ob
       else this
     }
 
-    def filter(f: A => Boolean): Option[A] = this match {
+    def orElse[B >: A](ob: => Option[B]): Option[B] =
+      this.map(Some(_)).getOrElse(ob)
+
+    def filterMatch(f: A => Boolean): Option[A] = this match {
       case Some(a) => if(f(a)) this else None
       case None => None
     }
+
+    def filter(f: A => Boolean): Option[A] = {
+      flatMap(a => if (f(a)) Some(a) else None)
+    }
+
   }
   case class Some[+A](get: A) extends Option[A]
   case object None extends Option[Nothing]
-
 
   // Exercise 4.2
   def mean(xs: Seq[Double]): Double = {
@@ -37,7 +49,57 @@ object Part1Section4 extends App {
   }
   def variance(xs: Seq[Double]): Option[Double] = {
     // This is hideous and I hate it
-    Some(mean(xs)) flatMap (m => Some(mean(xs.map(x => math.pow(x - m, 2)))))
+    Some(mean(xs)).flatMap(m => Some(mean(xs.map(x => math.pow(x - m, 2)))))
   }
+
+  // Exercise 4.3
+  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =  {
+    for {
+      aa <- a
+      bb <- b
+    } yield f(aa,bb)
+  }
+
+  // Exercise 4.4
+  def sequence[A](a: List[Option[A]]): Option[List[A]] = a match {
+    case Nil => Some(Nil)
+    case h :: t => if(h.getOrElse(None) == None) None else h.flatMap(hh => sequence(t).map(hh :: _))
+  }
+
+  // Exercise 4.5
+  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = a match {
+    case Nil => Some(Nil)
+    case h :: t => map2(f(h), traverse(t)(f))(_ :: _)
+  }
+
+  // Exercise 4.6
+  sealed trait Either[+E, +A] {
+    def map[B](f: A => B): Either[E, B] = this match {
+      case Left(e) => Left(e)
+      case Right(a) => Right(f(a))
+    }
+
+    def orElse[EE >: E,B >: A](b: => Either[EE, B]): Either[EE, B] = this match {
+      case Left(_) => b
+      case Right(a) => Right(a)
+    }
+
+    def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = this match {
+      case Left(e) => Left(e)
+      case Right(a) => f(a)
+    }
+
+    def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = {
+      for {
+        a <- this
+        bb <- b
+      } yield f(a, bb)
+    }
+
+  }
+  case class Left[+E](value: E) extends Either[E, Nothing]
+  case class Right[+A](value: A) extends Either[Nothing, A]
+
+  println(Right(1).map(i => i + 1))
 
 }
--- src/test/scala/Part1Section4Test.scala
+++ src/test/scala/Part1Section4Test.scala
@@ -1,4 +1,4 @@
-import Part1Section4.{None, Some, Option, variance}
+import Part1Section4.{None, Some, Option, variance, map2, sequence}
 import org.scalatest.FunSpec
 
 
@@ -6,8 +6,9 @@ class Part1Section4Test extends FunSpec {
   val addOne: (Int => Int) = (i: Int) => i + 1
   val threeDividedBy: (Int => Option[Int]) = (i: Int) => if(i == 0) None else Some(3 / i)
   val isThree: (Int => Boolean) = (i: Int) => i == 3
+  val add: ((Int, Int) => Int) = (i: Int, j: Int) => i + j
 
-  describe("Map") {
+  describe("Excercise 4.1 map") {
     it("should return the result of the function when something is provided") {
       assert(Some(3).map(addOne) == Some(4))
     }
@@ -16,7 +17,7 @@ class Part1Section4Test extends FunSpec {
     }
   }
 
-  describe("flatMap") {
+  describe("4.1 flatMap") {
     it("should return the result of the function when Some(_) is provided") {
       assert(Some(1).flatMap(threeDividedBy) == Some(3))
     }
@@ -28,7 +29,7 @@ class Part1Section4Test extends FunSpec {
     }
   }
 
-  describe("getOrElse") {
+  describe("Exercise 4.1 getOrElse") {
     it("should return the value if it is not None") {
       assert(Some(1).getOrElse(10) == 1)
     }
@@ -37,7 +38,7 @@ class Part1Section4Test extends FunSpec {
     }
   }
 
-  describe("orElse") {
+  describe("Exercise 4.1 orElse") {
     it("should return the called option when something is supplied") {
       assert(Some(1).orElse(Some(2)) == Some(1))
     }
@@ -46,7 +47,7 @@ class Part1Section4Test extends FunSpec {
     }
   }
 
-  describe("filter") {
+  describe("Exercise 4.1 filter") {
     it("should return itself if the condition is met") {
       assert(Some(3).filter(isThree) == Some(3))
     }
@@ -58,10 +59,30 @@ class Part1Section4Test extends FunSpec {
     }
   }
 
-  describe("variance") {
+  describe("Exercise 4.2 variance") {
     it("should calculate the variance of a given sequence") {
       assert(variance(Seq(1.0, 2.0, 3.0)) == Some(0.6666666666666666))
     }
   }
+
+  describe("Exercise 4.3 map2") {
+    it("should take a non-option function and and allow it to work the option") {
+      assert(map2(Some(1), Some(2))(add) == Some(3))
+    }
+
+    it("should return None if either parameters are None") {
+      assert(map2(None, Some(2))(add) == None)
+    }
+  }
+
+  describe("Exercise 4.4 sequence") {
+    it("should return an Option containing a list") {
+      assert(sequence(List(Some(1),Some(2),Some(3))) == Some(List(1,2,3)))
+    }
+    it("should return None if None is in the list") {
+      assert(sequence(List(Some(1),None,Some(3))) == None)
+    }
+  }
+
 
 }
